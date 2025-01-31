dedf9311c63b37d02a2378a478b58fcde9941b72
TBandi <TBandi@PRO-IND-1565.ad.prolifics.com>
2025-01-31 13:11:08 +0530

    first commit....

---
+++ src/Demo/Test1.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			System.out.print("eat something");
+		} else {
+			System.out.print("do work...");
+		}
+
+	}
+
+}
