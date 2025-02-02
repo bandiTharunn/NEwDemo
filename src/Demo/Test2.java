dc7c444c5d2e8fed89a023d8a842c89305b65f23
TBandi <TBandi@PRO-IND-1565.ad.prolifics.com>
2025-02-02 14:22:39 +0530

    I chnages the code look at it once

--- src/Demo/Test2.java
+++ src/Demo/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 
 		String hungry = "yes";
 
-		if (hungry == "yes") {
+		if (hungry == "yes" || hungry == "Y") {
 			System.out.print("eat something");
 		} else {
 			System.out.print("do work...");
